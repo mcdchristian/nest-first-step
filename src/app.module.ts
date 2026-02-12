1197bc067028e5da91e5abdfcc72ef39d8b9c328
mcdchristian <mcdchristian4@gmail.com>
2026-02-12 15:09:55 +0200

    update file

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,26 +10,13 @@ import { TodoModule } from './todo/todo.module';
 import { FirstMiddleware } from './middlewares/first/first.middleware';
 import { loggerMiddleware } from './middlewares/logger.middleware';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import dotenv from 'dotenv';
 
-dotenv.config();
+// import dotenv from 'dotenv';
+
+// dotenv.config();
 
 @Module({
-  imports: [
-    TodoModule,
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: process.env.DB_TYPE as 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT) || 3306,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: ['dist/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
-  ],
+  imports: [TodoModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
   exports: [AppService],
